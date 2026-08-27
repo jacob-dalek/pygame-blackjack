c5948a795dd0e0dd7aad18ac60812458dd0c7371
jacob-dalek <jacobsenier@gmail.com>
2026-08-27 17:48:54 +0100

    its a mess atm

--- blackjack.py
+++ blackjack.py
@@ -6,6 +6,11 @@ WIDTH = 640
 HEIGHT = 640
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+SUITS = ['C', 'D', 'H', 'S']
+FACE_VALUES = ['J','K','Q','A']
+VALUES = [2,3,4,5,6,7,8,9,10]
+
+
 class Card:
     def __init__(self, value, suit):
         self.value = value
@@ -14,20 +19,50 @@ class Card:
     def load_image(self):
         card = f"{os.getcwd()}/cards/{self.value}-{self.suit}.png" 
         image = pygame.image.load(card)
-        image = pygame.transform.scale(image,(180,180))
-        screen.blit(image,(0,HEIGHT-180))
+        # image = pygame.transform.scale(image,(180,180))
+        # screen.blit(image,(0,HEIGHT-180))
 
     def __str__(self):
         return f"{self.value} {self.suit}"
 
 def init_deck(deck: list[Card]):
-        suits = ['C', 'D', 'H', 'S']
-        values = [2,3,4,5,6,7,8,9,10,'J', 'K', 'Q', 'A'] # pythons weird typecasting
-        for suit in suits:
-            for value in values:
+        for suit in SUITS:
+            for value in VALUES + FACE_VALUES:
                 deck.append(Card(value, suit))
         random.shuffle(deck)
 
+
+class Entity:
+    def __init__(self, hand: list[Card], name=""):
+        self.__value = 0
+        self.name = name
+        self.hand = hand
+        self.ace_count = 0
+
+    
+
+    def hand_sum(self):
+        if not self.hand and len(self.hand) < 1:
+            return self.__value
+        sum = 0
+        for card in self.hand:
+            sum += card_logic(card.value, self)
+        self.__value = sum
+
+def card_logic(value, entity: Entity):
+        if value not in FACE_VALUES:
+            return value
+        if value == 'A':
+            ++entity.ace_count
+        else:
+            return 10
+
+class Dealer(Entity):
+    def __init__(self, hand, name=""):
+        super().__init__(hand, name)
+
+    def reveal_card(self):
+        self.hand[0].load_image()
 
 class Blackjack:
 
@@ -43,8 +78,9 @@ class Blackjack:
                 if event.type == pygame.QUIT:
                     running = False
 
-            screen.fill((0,255,0))
+            screen.fill((100,255,0))
 
+            print(deck[0])
             deck[0].load_image()
 
             pygame.display.flip()
